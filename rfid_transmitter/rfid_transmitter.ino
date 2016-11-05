67659d27d4fb78fea08787e4b468286d902d855d
DimaK <kurchigin.dmitry@yandex.ru>
2016-11-05 22:39:17 +0400

    send request

--- rfid_transmitter/rfid_transmitter.ino
+++ rfid_transmitter/rfid_transmitter.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <Ethernet2.h>
 
+int i = 0;
+
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress server(192, 168, 8, 2);  // numeric IP for Google (no DNS)
 //char server[] = "www.google.com";    // name address for Google (using DNS)
@@ -8,50 +10,33 @@ IPAddress ip(192, 168, 8, 8);
 EthernetClient client;
 
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
   Ethernet.begin(mac, ip);
-  //}
-  // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    //Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /test_params.pl?name=ardu&color=green HTTP/1.1");
-    client.println("Host: 192.168.8.2");
-    client.println("Connection: close");
-    client.println();
-  }
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
 }
 
 void loop()
 {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
+  delay(1000);
+  sendRequest();
+}
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true);
+void sendRequest() {
+  client.stop();
+  
+  if(client.connect(server, 80)) {
+    Serial.println("connected");
+    client.println("GET /test_params.pl?name=ardu88&color=nicegreen HTTP/1.1");
+    client.println("Host: 192.168.8.2");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    Serial.println("connection failed");
   }
 }
 
