c9402cb4e4834ad66bca0d319e3b2fb2c467e67d
DKurchigin <kurchigin.dmitry@yandex.ru>
2016-11-06 19:08:03 +0300

    add buttons

--- robot mk1/transmitter433/transmitter433.ino
+++ robot mk1/transmitter433/transmitter433.ino
@@ -8,12 +8,22 @@
 #define MOVE_LEFT "cm43"
 #define MOVE_RIGHT "cm44"
 
+#define FORWARD_BUTTON 7
+#define BACK_BUTTON 8
+#define LEFT_BUTTON 9
+#define RIGHT_BUTTON 10
+
 char *cont;
 int message;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN,OUTPUT);
+  pinMode(FORWARD_BUTTON, INPUT);
+  pinMode(BACK_BUTTON, INPUT);
+  pinMode(LEFT_BUTTON, INPUT);
+  pinMode(RIGHT_BUTTON, INPUT);
+  
   vw_set_ptt_inverted(true);
   vw_set_tx_pin(12);
   vw_setup(1000);
@@ -34,6 +44,20 @@ void loop() {
     } else if (message == '4') {
       send_message(MOVE_RIGHT);
     } 
+  }
+
+  boolean forwardUp = digitalRead(FORWARD_BUTTON);
+  boolean backUp = digitalRead(BACK_BUTTON);
+  boolean leftUp = digitalRead(LEFT_BUTTON);
+  boolean rightUp = digitalRead(RIGHT_BUTTON);
+  if (!forwardUp) {
+    send_message(MOVE_FORWARD);
+  } else if (!backUp) {
+    send_message(MOVE_BACK);
+  } else if (!leftUp) {
+    send_message(MOVE_LEFT);
+  } else if (!rightUp) {
+    send_message(MOVE_RIGHT);
   }
 }
 
