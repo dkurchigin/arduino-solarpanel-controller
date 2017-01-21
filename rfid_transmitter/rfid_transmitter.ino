d325d55cbd4e37b7269851caf7e4a4bdf8fb9919
DKurchigin <kurchigin.dmitry@yandex.ru>
2017-01-21 18:59:26 +0300

    add reading

--- rfid_transmitter/rfid_transmitter.ino
+++ rfid_transmitter/rfid_transmitter.ino
@@ -1,17 +1,24 @@
 #include <SPI.h>
 #include <Ethernet2.h>
+#include <MFRC522.h>
 
 #define BUTTON 9
 #define BUZZER 3
 #define GREEN_LED 5
 #define RED_LED 4
 
+#define SS_PIN 8
+#define RST_PIN 7
+
 boolean buttonWasUp = true;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress server(192, 168, 8, 2); 
 IPAddress ip(192, 168, 8, 8);
-
 EthernetClient client;
+
+MFRC522 rfid(SS_PIN, RST_PIN); 
+MFRC522::MIFARE_Key key;
+byte nuidPICC[3];
 
 void setup() {
   Serial.begin(9600);
@@ -26,11 +33,19 @@ void setup() {
   Ethernet.begin(mac, ip);
   delay(1000);
   Serial.println("connecting...");
+
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
 }
 
-void loop()
-{
+void loop() {
   boolean buttonUp = digitalRead(BUTTON);
+  
   if (!buttonUp && buttonWasUp) {
     delay(10);
     buttonUp = digitalRead(BUTTON);
@@ -42,6 +57,48 @@ void loop()
       digitalWrite(GREEN_LED, LOW);
     }
   }
+
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+   
+    Serial.println(F("The NUID tag is:"));
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+  else {
+    Serial.println(F("Card read previously."));
+    sendPost();
+  }
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+  
   buttonWasUp = buttonUp;
 }
 
@@ -57,7 +114,6 @@ void sendRequest() {
     Serial.println("connection failed");
   }
 }
-
 
 void sendPost() {
   client.stop();
@@ -78,4 +134,11 @@ void sendPost() {
   } else {
     Serial.println("connection failed");
   }  
+}
+
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
 }
